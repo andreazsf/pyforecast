bdf7516c046e7147459449f629a4341464ad3941
Andrea San Felipe <andrea.sanfelipe@gmail.com>
2024-02-22 02:54:25 +0800

    Completed sign in page + firebase authentication.

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,19 +1,27 @@
-
 const routes = [
   {
-    path: '/',
-    component: () => import('layouts/MainLayout.vue'),
+    path: "/",
+    component: () => import("layouts/AuthLayout.vue"),
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue') }
-    ]
+      { path: "/", redirect: "/login" },
+      { path: "login", component: () => import("src/pages/LoginPage.vue") },
+      {
+        path: "register",
+        component: () => import("src/pages/RegisterPage.vue"),
+      },
+    ],
+    meta: { auth: false },
   },
-
-  // Always leave this as last one,
-  // but you can also remove it
   {
-    path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue')
-  }
-]
+    path: "/dashboard",
+    component: () => import("layouts/MainLayout.vue"),
+    children: [{ path: "", component: () => import("pages/IndexPage.vue") }],
+    meta: { auth: true },
+  },
+  {
+    path: "/:catchAll(.*)*",
+    component: () => import("pages/ErrorNotFound.vue"),
+  },
+];
 
-export default routes
+export default routes;
